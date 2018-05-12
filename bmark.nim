d705559dc5ee8f6902addfe5460c0b7d616edf0a
Eli <elimaynard923@gmail.com>
2018-05-12 15:59:46 -0400

    Two-color optimization. ~1.3x faster.

---
+++ bmark.nim
@@ -0,0 +1,26 @@
+
+import times
+import os
+import strutils
+import stats
+
+# https://stackoverflow.com/questions/36577570/how-to-benchmark-few-lines-of-code-in-nim
+template benchmark*(benchmarkName: string, trials = 10, code: typed) =
+    var rs: RunningStat
+    var total = 0.0
+
+    for trial in 0 ..< trials:
+        let t0 = epochTime()
+        code
+        let elapsed = epochTime() - t0
+        total += elapsed
+        rs.push(elapsed)
+
+    echo("Benchmark [$1]: n = $2, mean = $3s, stdev = $4s, total = $5s" % [
+        benchmarkName,
+        $trials,
+        $rs.mean().formatFloat(format = ffDecimal, precision = 5),
+        $rs.standardDeviationS().formatFloat(format = ffDecimal, precision = 5),
+        $total.formatFloat(format = ffDecimal, precision = 5),
+    ])
+
--- coloring.nim
+++ coloring.nim
@@ -3,19 +3,29 @@ import strutils
 import sequtils
 import math
 
+# Compile-time symbol `noTwoColorOptim` may be used to disable
+# two-color optimizations
+
 type Coloring*[C: enum, S: static[int]] = array[S, C]
 
-proc initColoring[C: enum, S: static[int]](): Coloring[C, S] =
+proc initColoring*[C: enum, S: static[int]](): Coloring[C, S] =
     var res: array[S, C]
     return Coloring[C, S](res)
 
 proc `+=`*[C: enum, S: static[int]](col: var Coloring[C, S], amt: int) =
-    const count = C.high.ord + 1
-    var overflow = amt
-    for n in 0 ..< S:
-        let val = col[n].ord
-        col[n] = C((val + overflow) mod count)
-        overflow = (val + overflow) div count
+    when C.high.ord == 1 and not defined(noTwoColorOptim): # two-colorings
+        var overflow = false
+        for n in 0 ..< S:
+            let s = ((amt shr n) and 1) + cast[int](overflow) + col[n].ord
+            col[n] = C(s mod 2)
+            overflow = s >= 2
+    else:
+        const count = C.high.ord + 1
+        var overflow = amt
+        for n in 0 ..< S:
+            let val = col[n].ord
+            col[n] = C((val + overflow) mod count)
+            overflow = (val + overflow) div count
 
 proc `$`*[C: enum, S: static[int]](col: Coloring[C, S]): string =
     result = "col["
@@ -26,4 +36,23 @@ proc `$`*[C: enum, S: static[int]](col: Coloring[C, S]): string =
             result &= ", " & $item
     result &= "]"
 
+
+when isMainModule:
+    import bmark
+
+    type Col = enum c0, c1
+    const len = 16 # Length of sequence
+    var col = initColoring[Col, len]()
+
+    let incsizes = toSeq(1 .. 2^len)
+    let inccounts = incsizes.map(proc(incsize: int): int = return ((2^len) div incsize))
+    benchmark("two-coloring", trials = 100):
+        for i, incsize in incsizes:
+            # Test incrementing with all values from 1 to 2^len, which is an instant overflow
+            for _ in 0 ..< inccounts[i]:
+                # Increment enough times for overflow
+                col += incsize
+            # At end, reset
+            for i in 0 ..< len:
+                col[i] = c0
 
