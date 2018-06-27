5b7460a458582c06d099398e561c4568f832dd24
Eli <elimaynard923@gmail.com>
2018-06-27 13:46:04 -0400

    Added CSV io.

---
+++ io.nim
@@ -0,0 +1,38 @@
+
+import strutils
+import sequtils
+import tables
+
+import misc
+
+# Simple CSV output implementation
+
+# Values are separated by commas
+# Commas are escaped with \
+# Backslahses are escaped with \
+# Quoutes are escaped wth \
+# If a value contains a newline, it will be surrounded by quotes
+
+proc writeRow*(file: File, vals: varargs[string, `$`]) =
+    file.writeln(
+        vals.map(proc(s: string): string =
+            var s = s.replaceMany({
+                ",": "\\,",
+                "\\": "\\\\",
+                "\"": "\\\"",
+            }.toTable)
+            if '\c' in s or '\L' in s:
+                s = "\"" & s & "\""
+            return s)
+
+            .join(","))
+
+when isMainModule:
+    let f = open("test.txt", fmWrite)
+    f.writeRow(1, 2, 3)
+    f.writeRow(4, 5, 6)
+    f.writeRow("a\nb")
+    f.writeRow("value before", "hello\non a new line!", "value after")
+    f.writeRow("I contain a couple of quotes! \" \" \" :)))")
+    f.writeRow("I have a backs\\ash!")
+
--- misc.nim
+++ misc.nim
@@ -1,9 +1,32 @@
 
 import random
+import tables
+import strutils
 
 random.randomize()
 
 var localRand = initRand(rand(int.high))
 proc rand_u64*(): uint64 =
     return localRand.next()
+
+# -- #
+
+func replaceMany*(s: string, repl: Table[string, string]): string =
+    ## Behaviour is undefined if duplicate keys exist
+    result = ""
+
+    var i = 0
+    while i < s.len:
+        block continueOuter:
+            for term in repl.keys:
+                if s.continuesWith(term, i):
+                    result.add(repl[term])
+                    i += term.len
+                    break continueOuter
+            result.add(s[i])
+            inc(i)
+
+when isMainModule:
+    assert("cool wow nice" == "nice cool wow".replaceMany({"nice": "cool", "cool": "wow", "wow": "nice"}.toTable))
+    assert("abc" == "123".replaceMany({"1": "a", "2": "b", "3": "c"}.toTable))
 
