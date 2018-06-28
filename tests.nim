5d71579bc5de8e320688a4dccd719a2c87b417a7
DragonRagers <dragonragers@gmail.com>
2018-06-28 14:45:28 -0400

    added some MAS tests

--- tests.nim
+++ tests.nim
@@ -8,12 +8,13 @@ import sets
 import os
 import terminal
 
+import coloring
 import twoColoring
-
+import find
 
 
 suite "Testing twoColoring":
-  var emptyTwoColoring, randomTwoColoring, fromStringColoring1, fromStringColoring2: TwoColoring
+  var emptyTwoColoring, randomTwoColoring, fromStringColoring1, fromStringColoring2, MAScoloring, noMAScoloring: TwoColoring
 
   setup:
     emptyTwoColoring = initTwoColoring(64)
@@ -21,7 +22,8 @@ suite "Testing twoColoring":
     randomize(randomTwoColoring)
     fromStringColoring1 = fromString("10110")
     fromStringColoring2 = fromString("10110")
-
+    MAScoloring = fromString("10011010111010")
+    noMAScoloring = fromString("0101101010011101100")
   teardown:
     echo "run after each test"
 
@@ -40,5 +42,9 @@ suite "Testing twoColoring":
     check(fromStringColoring1[2] == 1)
     check(fromStringColoring1[3] == 1)
     check(fromStringColoring1[4] == 0)
+
+  test "has_MAS":
+    check(has_MAS(cast [Coloring[2]](MAScoloring), 5))
+    check(not has_MAS(cast [Coloring[2]](noMAScoloring), 5))
 
   echo "suite teardown: run once after the tests"
