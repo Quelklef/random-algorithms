9f7352db5df9ea370366cf441ff7c249dd349f43
DragonRagers <dragonragers@gmail.com>
2018-07-16 16:33:22 -0400

    Created node type with position variable

---
+++ node.nim
@@ -0,0 +1,43 @@
+type
+  NodeObj = ref object
+    name*: string
+    position*: int64
+    vertices*: seq[Node]
+  Node = ref NodeObj
+
+func initNode*(n: string): Node =
+  result.name = n
+  result.position = -1
+  result.vertices = @[]
+
+func setPosition*(n: var Node, i: int64): void =
+  n.position =  i
+
+func addVertex*(n1: var Node, n2: Node): void =
+    n1.vertices.add(n2)
+
+func connected*(n1: Node, n2: Node): bool =
+  return n2 in n1.vertices
+
+func right(n1: Node, n2: Node): bool =
+  if n1.position < n2.position:
+    return true
+  else:
+    return false
+
+func left(n1: Node, n2: Node): bool =
+  return not right(n1, n2)
+
+#checks if any connected vertices are to the right, true if there are
+func testRight*(n: Node): bool =
+  for i in n.vertices:
+    if right(n, i):
+      return true
+  return false
+
+#testRight but testLeft
+func testLeft*(n: Node): bool =
+  for i in n.vertices:
+    if left(n, i):
+      return true
+  return false
