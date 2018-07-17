151887b18955b43e93b7d5d23accfd5e6536064b
DragonRagers <dragonragers@gmail.com>
2018-07-17 18:12:03 -0400

    Changed output to implement io.nim

--- Graph/graph.nim
+++ Graph/graph.nim
@@ -2,6 +2,9 @@ import node
 import random
 import sequtils
 import math
+import tables
+import io
+import misc
 
 type Graph = object
   nodes: seq[Node]
@@ -17,15 +20,17 @@ func seqNodes(num: int): seq[Node] =
 proc initGraph(n: int): Graph =
   result.nodes = seqNodes(n)
 
-proc shuffle*(g: Graph): void =
-  var nums: seq[int]
-  for i in 0 ..< size(g):
-    nums.add(i) #sequence from 0 to number of nodes - 1
+proc shuffle*(g: var Graph): void =
+  var newSeq: seq[Node] #new sequence to replace g.nodes
+  newSeq = @[]
   var pos: int
-  for n in g.nodes: #pick a random index in nums, assign value at index to node, remove that index from nums
-    pos = rand(nums.len-1)
-    n.position = nums[pos]
-    nums.del(pos)
+  for i in 0 ..< size(g): #pick a random node in g.node and add to newSeq, then remove it from g.nodes
+    pos = rand(size(g)-1)
+    newSeq.add(g.nodes[pos])
+    g.nodes.del(pos)
+  g.nodes = newSeq #replace g.nodes with newSeq
+  for i, n in g.nodes: #assign nodes position in accending order
+    n.position = i
 
 #makes simple graph
 #multigraphs can be treated as simple graphs for independent sets
@@ -48,25 +53,33 @@ func shuffle*(g: Graph) =
   for pair in zip(g.nodes, nums):
     setPosition(pair.a, pair.b)
 
-proc shuffle*(g: var Graph): void =
-  var newSeq: seq[Node] #new sequence to replace g.nodes
-  newSeq = @[]
+proc shuffle*(g: Graph): void =
+  var nums: seq[int]
+  for i in 0 ..< size(g):
+    nums.add(i) #sequence from 0 to number of nodes - 1
   var pos: int
-  for i in 0 ..< size(g): #pick a random node in g.node and add to newSeq, then remove it from g.nodes
-    pos = rand(size(g)-1)
-    newSeq.add(g.nodes[pos])
-    g.nodes.del(pos)
-  g.nodes = newSeq #replace g.nodes with newSeq
-  for i, n in g.nodes: #assign nodes position in accending order
-    n.position = i
+  for n in g.nodes: #pick a random index in nums, assign value at index to node, remove that index from nums
+    pos = rand(nums.len-1)
+    n.position = nums[pos]
+    nums.del(pos)
+
 ]#
+let tabular = initTabular(
+    ["Position", "Name", "Connected to"],
+    [2         , 1     , 10],
+)
+
+proc report(values: varargs[string, `$`]) =
+    echo tabular.row(values)
 
 proc toString*(g: Graph): string =
+  echo tabular.title()
   for n in g.nodes:
     var edges = ""
     for e in n.vertices:
       edges.add(" " & e.name)
-    result.add("\n" & n.name & " (" & $n.position & "):" & edges)
+    #result.add("\n" & $n.position & " (" & n.name & "):" & edges)
+    report(n.position, n.name, edges)
 
 func findIndSetRight*(g: Graph): seq[Node] =
   for n in g.nodes:
