3624fb0102a6a46b94c0744f92e13f5c195becc2
DragonRagers <dragonragers@gmail.com>
2018-07-17 17:21:15 -0400

    actually added randomization

--- Graph/graph.nim
+++ Graph/graph.nim
@@ -10,7 +10,7 @@ func size*(g: Graph): int =
   return g.nodes.len
 
 #makes a sequence of nodes with names a, b, c, etc
-func seqNodes(num: int64): seq[Node] =
+func seqNodes(num: int): seq[Node] =
   for i in 0 ..< num:
     result.add(initNode((chr(ord('a') + i))))
 
@@ -21,19 +21,21 @@ proc initRandGraph*(n: int): Graph =
   #n-1 is min num of edges in a graph, n*(n-1)/2 is max
   #returns random number inbetween min and max
   var numEdges = rand(int(n*(n-1)/2) - (n-1)) + (n-1)
-  for i in 0 ..< n :
-    var pair = zip(result.nodes, result.nodes)[i]
-    addVertex(pair.a, pair.b)
-    addVertex(pair.b, pair.a)
+  for i in 0 ..< n:
+    var n1 = rand(n-1)
+    var n2 = rand(n-1)
+    addVertex(result.nodes[n1], result.nodes[n2])
+    addVertex(result.nodes[n2], result.nodes[n1])
 
 #shuffles the positions of all the nodes within g
+#[
 func shuffle*(g: Graph) =
   let nums = toSeq(0 ..< g.size)
   for pair in zip(g.nodes, nums):
     setPosition(pair.a, pair.b)
 
-#[
-proc shuffle1*(g: Graph): void =
+
+proc shuffle*(g: Graph): void =
   var nums: seq[int]
   for i in 0 ..< size(g):
     nums.add(i) #sequence from 0 to number of nodes - 1
@@ -42,8 +44,8 @@ proc shuffle1*(g: Graph): void =
     pos = rand(nums.len)
     n.position = nums[pos]
     nums.delete(pos)
-
-proc shuffle2*(g: var Graph): void =
+]#
+proc shuffle*(g: var Graph): void =
   var newSeq: seq[Node] #new sequence to replace g.nodes
   newSeq = @[]
   var pos: int
@@ -54,7 +56,7 @@ proc shuffle2*(g: var Graph): void =
   g.nodes = newSeq #replace g.nodes with newSeq
   for i, n in g.nodes: #assign nodes position in accending order
     n.position = i
-]#
+
 
 proc toString*(g: Graph): string =
   for n in g.nodes:
@@ -73,10 +75,12 @@ func findIndSetLeft*(g: Graph): seq[Node] =
     if testLeft(n):
         result.add(n)
 
-func iSet*(both: bool, g: Graph): seq[int] =
- var l = findIndSetLeft(g).len
- var r = findIndSetRight(g).len
- if both:
-  return @[l, r]
- else:
-   return @[r]
+func iSet*(g: Graph): seq[int] =
+ return @[findIndSetLeft(g).len, findIndSetRight(g).len]
+
+#[
+func iSet*(g: Graph): int =
+  shuffle(g)
+  result = findIndSetLeft(g).len
+  #result = findIndSetRight(g).len
+]#
