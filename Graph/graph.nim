6da8db18783921ce56930f590fa51229d53ab66c
DragonRagers <dragonragers@gmail.com>
2018-07-17 17:40:43 -0400

    fixed shuffle and initRandGraph

--- Graph/graph.nim
+++ Graph/graph.nim
@@ -14,6 +14,9 @@ func seqNodes(num: int): seq[Node] =
   for i in 0 ..< num:
     result.add(initNode((chr(ord('a') + i))))
 
+proc initGraph(n: int): Graph =
+  result.nodes = seqNodes(n)
+
 #makes simple graph
 #multigraphs can be treated as simple graphs for independent sets
 proc initRandGraph*(n: int): Graph =
@@ -26,14 +29,14 @@ proc initRandGraph*(n: int): Graph =
     var n2 = rand(n-1)
     addVertex(result.nodes[n1], result.nodes[n2])
     addVertex(result.nodes[n2], result.nodes[n1])
-
+    
 #shuffles the positions of all the nodes within g
 #[
 func shuffle*(g: Graph) =
   let nums = toSeq(0 ..< g.size)
   for pair in zip(g.nodes, nums):
     setPosition(pair.a, pair.b)
-
+]#
 
 proc shuffle*(g: Graph): void =
   var nums: seq[int]
@@ -41,29 +44,29 @@ proc shuffle*(g: Graph): void =
     nums.add(i) #sequence from 0 to number of nodes - 1
   var pos: int
   for n in g.nodes: #pick a random index in nums, assign value at index to node, remove that index from nums
-    pos = rand(nums.len)
+    pos = rand(nums.len-1)
     n.position = nums[pos]
-    nums.delete(pos)
-]#
+    nums.del(pos)
+#[
 proc shuffle*(g: var Graph): void =
   var newSeq: seq[Node] #new sequence to replace g.nodes
   newSeq = @[]
   var pos: int
   for i in 0 ..< size(g): #pick a random node in g.node and add to newSeq, then remove it from g.nodes
-    pos = rand(size(g))
+    pos = rand(size(g)-1)
     newSeq.add(g.nodes[pos])
-    g.nodes.delete(pos)
+    g.nodes.del(pos)
   g.nodes = newSeq #replace g.nodes with newSeq
   for i, n in g.nodes: #assign nodes position in accending order
     n.position = i
-
+]#
 
 proc toString*(g: Graph): string =
   for n in g.nodes:
     var edges = ""
     for e in n.vertices:
       edges.add(" " & e.name)
-    result.add("\n" & n.name & ":" & edges)
+    result.add("\n" & n.name & "(" & $n.position & "):" & edges)
 
 func findIndSetRight*(g: Graph): seq[Node] =
   for n in g.nodes:
