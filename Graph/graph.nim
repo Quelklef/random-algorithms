2bf4aa6bd4daad84fe3082203a906449b21d23ef
DragonRagers <dragonragers@gmail.com>
2018-07-17 15:32:20 -0400

    finalized shuffle method and created random graph generator

--- Graph/graph.nim
+++ Graph/graph.nim
@@ -1,5 +1,7 @@
 import node
 import random
+import sequtils
+import math
 
 type Graph = object
   nodes: seq[Node]
@@ -7,23 +9,44 @@ type Graph = object
 func size*(g: Graph): int =
   return g.nodes.len
 
+#makes a sequence of nodes with names a, b, c, etc
+func seqNodes(num: int64): seq[Node] =
+  for i in 0 ..< num:
+    result.add(initNode((chr(ord('a') + i))))
+
+#makes simple graph
+#multigraphs can be treated as simple graphs for independent sets
+proc initRandGraph*(n: int): Graph =
+  result.nodes = seqNodes(n)
+  #n-1 is min num of edges in a graph, n*(n-1)/2 is max
+  #returns random number inbetween min and max
+  var numEdges = rand(int(n*(n-1)/2) - (n-1)) + (n-1)
+  for i in 0 ..< n :
+    var pair = zip(result.nodes, result.nodes)[i]
+    addVertex(pair.a, pair.b)
+    addVertex(pair.b, pair.a)
+
 #shuffles the positions of all the nodes within g
-func shuffle1*(g: Graph): void =
+func shuffle*(g: Graph) =
+  let nums = toSeq(0 ..< g.size)
+  for pair in zip(g.nodes, nums):
+    pair.a.position = pair.b
+
+#[
+proc shuffle1*(g: Graph): void =
   var nums: seq[int]
   for i in 0 ..< size(g):
     nums.add(i) #sequence from 0 to number of nodes - 1
   var pos: int
-  random.randomize()
   for n in g.nodes: #pick a random index in nums, assign value at index to node, remove that index from nums
     pos = rand(nums.len)
     n.position = nums[pos]
     nums.delete(pos)
 
-func shuffle2*(g: var Graph): void =
+proc shuffle2*(g: var Graph): void =
   var newSeq: seq[Node] #new sequence to replace g.nodes
   newSeq = @[]
   var pos: int
-  random.randomize()
   for i in 0 ..< size(g): #pick a random node in g.node and add to newSeq, then remove it from g.nodes
     pos = rand(size(g))
     newSeq.add(g.nodes[pos])
@@ -31,6 +54,7 @@ func shuffle2*(g: var Graph): void =
   g.nodes = newSeq #replace g.nodes with newSeq
   for i, n in g.nodes: #assign nodes position in accending order
     n.position = i
+]#
 
 func findIndSetRight*(g: Graph): seq[Node] =
   for n in g.nodes:
@@ -41,3 +65,19 @@ func findIndSetLeft*(g: Graph): seq[Node] =
   for n in g.nodes:
     if testLeft(n):
         result.add(n)
+
+func iSet*(both: bool, g: Graph): seq[int] =
+ var l, r = 0
+ # insert shuffle function
+ if both:
+  for i in g.nodes:
+   if not testLeft(i):
+    inc(l)
+   if not testRight(i):
+    inc(r)
+  result = @[l, r]
+ else:
+    for i in g.nodes:
+     if not testLeft(i):
+       inc(l)
+    result.add(l)
