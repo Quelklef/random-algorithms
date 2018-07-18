15dbca7b23cfba57f94a8df4f57e57cbae51ba20
DragonRagers <dragonragers@gmail.com>
2018-07-18 18:54:26 -0400

    Can specify n and e or only n

--- Graph/Turan.nim
+++ Graph/Turan.nim
@@ -4,12 +4,18 @@ import random
 
 random.randomize()
 
-proc turan*(n: int, e: int): void =
+proc turan*(n: int, e: int = -1): void =
   var edges = e
   if float(e) > n*(n-1)/2:
     edges = int(n*(n-1)/2)
     echo "Too many edges, defaulting to ", edges, " edges"
-  var g = initRandGraph(n, edges)
+
+  var g: Graph
+  if e < 0:
+    g = initRandGraph(n)
+  else:
+    g = initRandGraph(n, edges)
+
   var turanNum = float(size(g))/(2*numE(g)/size(g) + 1)
 
   echo "n = ", size(g), ", e = ", numE(g)
@@ -22,4 +28,4 @@ proc turan*(n: int, e: int): void =
   echo "Shuffled ", count, " times"
   echo toString(g)
 
-turan(10, 30)
+turan(30, 50)
