1426b5aecf38cd99a0d24f7ae381288d688261c1
DragonRagers <dragonragers@gmail.com>
2018-07-18 18:30:02 -0400

    created basic Turan Theorm testing

---
+++ Graph/Turan.nim
@@ -0,0 +1,25 @@
+import node
+import graph
+import random
+
+random.randomize()
+
+proc turan*(n: int, e: int): void =
+  var edges = e
+  if float(e) > n*(n-1)/2:
+    edges = int(n*(n-1)/2)
+    echo "Too many edges, defaulting to ", edges, " edges"
+  var g = initRandGraph(n, edges)
+  var turanNum = float(size(g))/(2*numE(g)/size(g) + 1)
+
+  echo "n = ", size(g), ", e = ", numE(g)
+  echo "Turan's Theorm: n/(2e/n + 1) = ", turanNum
+
+  var count = 1
+  while float(iSet(g)) < turanNum:
+    count += 1
+    shuffle(g)
+  echo "Shuffled ", count, " times"
+  echo toString(g)
+
+turan(10, 30)
