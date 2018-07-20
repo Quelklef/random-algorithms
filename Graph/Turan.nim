4e49f5213cf2fc39d6586c50887d21200fe8680c
Gabriel N <gabrielnaval2001@gmail.com>
2018-07-20 15:10:27 -0400

    added func that just returns num shuffles taken

--- Graph/Turan.nim
+++ Graph/Turan.nim
@@ -4,7 +4,7 @@ import random
 
 random.randomize()
 
-proc turan*(n: int, e: int = -1): void =
+proc turanToString*(n: int, e: int = -1): void =
   var edges = e
   if float(e) > n*(n-1)/2:
     edges = int(n*(n-1)/2)
@@ -27,5 +27,21 @@ proc turan*(n: int, e: int = -1): void =
     shuffle(g)
   echo "Shuffled ", count, " times"
   echo toString(g)
+
+proc turan*(n: int, e: int = -1): int =
+  var edges = e
+  if float(e) > n*(n-1)/2:
+    edges = int(n*(n-1)/2)
+    echo "Too many edges, defaulting to ", edges, " edges"
+  var g: Graph
+  if e < 0:
+    g = initRandGraph(n)
+  else:
+    g = initRandGraph(n, edges)
+  var turanNum = float(size(g))/(2*numE(g)/size(g) + 1)
+  return = 1
+  while float(iSet(g)) < turanNum:
+    return += 1
+    shuffle(g)
 
 turan(30, 50)
