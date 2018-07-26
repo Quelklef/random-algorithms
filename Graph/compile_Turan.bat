31d1ed2e136593422ed442e69de26c2aff9ef4c0
DragonRagers <dragonragers@gmail.com>
2018-07-26 02:29:52 -0400

    aahhh so cool

--- Graph/compile_Turan.bat
+++ Graph/compile_Turan.bat
@@ -1,8 +1,9 @@
 del Turan.exe
 sleep 1
 nim c --threads:on -d:release Turan
-Turan.exe 10 .05 10000
-Turan.exe 20 .05 10000
-Turan.exe 30 .05 10000
-Turan.exe 40 .05 10000
+Turan.exe 10 .05 50000
+Turan.exe 20 .05 50000
+Turan.exe 30 .05 50000
+Turan.exe 40 .05 50000
+Turan.exe 50 .05 50000
 pause
