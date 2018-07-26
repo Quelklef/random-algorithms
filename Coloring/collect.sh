6b52741bffc36c58eb560e9d36249af1de373b4b
Eli <elimaynard923@gmail.com>
2018-07-25 20:08:16 -0400

    Add data collection script

---
+++ Coloring/collect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" != 2 ]; then
+  echo "Requires two args: C, K"
+  exit
+fi
+
+N="$(ls | wc -l)"
+result=""
+pushd data/"C_$(printf %05d $1)__K_$(printf %05d $2)"
+i=1
+for filename in ./*; do
+  echo "Collecting from $filename"
+  result="$result
+$(cat $filename | sed "s/^/$i,/g")"
+  i="$(($i + 1))"
+done
+
+popd
+echo "$result" > datacollected.txt
