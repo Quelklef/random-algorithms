d53473039222647cb78e96bcdd75f24d455e289a
Eli <elimaynard923@gmail.com>
2019-01-25 07:59:38 -0500

    Remove Turan's Thm code

--- Graph/compile_Turan.bat
+++
@@ -1,8 +0,0 @@
-del Turan.exe
-sleep 1
-nim c --threads:on -d:release Turan
-for /l %%x in (10,10,100) do (
-  Turan.exe %%x .01 2500 1
-)
-pause
-nim c -r Stat
--- Graph/test.nim
+++
@@ -1,8 +0,0 @@
-import Turan
-import graph
-import node
-
-var g: Graph = initRandGraph(20, 10)
-shuffle(g)
-echo iSet(g)
-echo greedyISet(g)
